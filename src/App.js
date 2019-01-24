ddba5d2cb40c896912603bcee2dc21823f962aed
Fabio Moretti <fmoretti981@gmail.com>
2019-01-24 12:34:30 +0100

    Bugfix and Stable Build

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import Router, {sceneReducer} from './Router';
 import { signInWithoutPasswordAction, configureStore } from 'core/lib';
 
 const store = configureStore({routes: sceneReducer});
-store.dispatch(signInWithoutPasswordAction());
+// store.dispatch(signInWithoutPasswordAction());
 
 export default class App extends Component {
     render() {
+        store.dispatch(signInWithoutPasswordAction());
         return (
             <Provider store={store}>
                 <Router/>
